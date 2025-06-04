7b2ec3968d1ad36c7686050bab8565d3d94da8e1
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-04 15:01:36 +0800

    chore: updates

--- Modules/App.py
+++ Modules/App.py
@@ -1,9 +1,19 @@
 import converters #1
-from converters import kgs_to_lbs #2
+from converters import kgs_to_lbs, test_numbers as test_num #2
 from utils import find_max
+import converters as cvt #3
+
+import sys
 
 print(converters.kgs_to_lbs(70)) #1
 print(kgs_to_lbs(70)) #2
+print(cvt.kgs_to_lbs(70))  #3
 
 numbers = [1, 3, 6, 2, 7, 10, 2, 5]
 print(find_max(numbers))
+
+print(converters.test_numbers) #1
+print(test_num) #2
+print(cvt.test_numbers) #3
+
+print(sys.path)
--- Modules/converters.py
+++ Modules/converters.py
@@ -1,5 +1,8 @@
+print('converters imported successfully!!')
 def lbs_to_kg(weight):
     return round(weight * 0.45359237,2)
 
 def kgs_to_lbs(weight):
     return round(weight / 0.45359237,2)
+
+test_numbers = [1, 3, 6, 2, 7, 10, 2, 5]
