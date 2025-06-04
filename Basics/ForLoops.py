4ace9b409b0f21bac5005e912924c393e7f0b6da
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-04 14:56:57 +0800

    update: add more examples for ForLoops

--- Basics/ForLoops.py
+++ Basics/ForLoops.py
@@ -1,19 +1,45 @@
+# for loop String
+print("for loop String:")
 for char in "Python":
     print(char)
 
+# for loop break
+print("\nfor loop break:")
+nums = [1, 2, 3, 4, 5]
+for num in nums:
+    if num == 3:
+        print("Found!")
+        break
+    print(num)
+
+# for loop continue
+print("\nfor loop continue:")
+nums = [1, 2, 3, 4, 5]
+for num in nums:
+    if num == 3:
+        print("Skipped!")
+        continue
+    print(num)
+
+# for loop list
+print("\nfor loop list:")
 names = ["Mosh", "John", "Sarah"]
 for name in names:
     print(name)
 
+for num in [6, 7, 8, 9]:
+    print(num)
+
+# for loop range
+print("\nfor loop range:")
 for num in range(0, 6):
     print(num)
-
-for num in [6,7,8,9]:
-    print(num)
-
+print()
 for num in range(10, 16):
     print(num)
 
+# for loop range, step
+print("\nfor loop range step:")
 for num in range(5, 10, 2):
     print(num)
 
@@ -24,3 +50,9 @@ for price in prices:
 print(f"Total = {total}")
 total2 = sum(prices)
 print(f"Total = {total2}")
+
+# loop within a loop
+print("\nloop within a loop:")
+for num in nums:
+    for letter in 'abc':
+        print(num, letter)
