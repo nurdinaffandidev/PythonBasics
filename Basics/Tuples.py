1fb5f44447b3c124baa36f4f3bfa0feb5d75c370
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-04 14:58:40 +0800

    update: add more examples for tuples

--- Basics/Tuples.py
+++ Basics/Tuples.py
@@ -16,3 +16,30 @@ print(x, y, z)
 list_coordinates = [4, 5, 6]
 x, y, z = list_coordinates
 print(x, y, z)
+
+
+# empty tuple init
+empty_tuple = ()
+empty_tuple2 = tuple()
+
+'''
+Mutability proof
+'''
+
+# Mutable
+list_1 =['History', 'Math', 'Science']
+list_2 = list_1
+print(list_1)
+print(list_2)
+list_1[0] = 'Art'
+print(list_1)
+print(list_2)
+
+# Immutable
+tuple_1 = ('History', 'Math', 'Science')
+tuple_2 = tuple_1
+print(tuple_1)
+print(tuple_2)
+# tuple_1[0] = 'Art' # will result in `TypeError: 'tuple' object does not support item assignment`
+# print(tuple_1)
+# print(tuple_2)
