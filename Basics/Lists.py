9903bcca9bbc75c6518dd058e84f6bc60f3aa96b
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-04 14:57:58 +0800

    update: add more examples for list functions

--- Basics/Lists.py
+++ Basics/Lists.py
@@ -1,12 +1,71 @@
 names = ["John", "Bob", "Mosh", "Mary", "Sarah"]
+# Accessing list value by index
 print(names[0])
 print(names[2])
 print(names[-1])
 print(names[-2])
 
+# Slicing
 print(names[2:]) # return new list
 print(names[2:4]) # return new list
 print(names)
+
+# check item in list
+print(f"2 in numbers = {'John' in names}")
+
+# append
+names.append("Josh1")
+print(names)
+
+# remove
+names.remove("Josh1")
+print(names)
+
+# insert
+names.insert(0, "Josh2")
+print(names)
+
+# pop
+popped_name = names.pop(0)
+print(f"popped name = {popped_name}")
+print(names)
+
+# extends list
+names2 = ["Ali", "Baba"]
+names.extend(names2)
+print(names)
+
+# sort
+names.sort()
+print(names)
+
+# sort reverse
+names.sort(reverse=True)
+print(names)
+
+# sorted copy
+names_sorted = sorted(names)
+print(names_sorted)
+print(names)
+
+# reverse sorted copy
+names.sort()
+names_reversed = sorted(names, reverse=True)
+print(names_reversed)
+print(names)
+
+# join
+names_joined = ', '.join(names)
+print(names)
+print(names_joined)
+
+# split
+list_split = names_joined.split(', ')
+print(list_split)
+
+# empty list init
+empty_list = []
+empty_list2 = list()
 
 numbers = [2, 10, 3, 4, 6, 7, 5, 9, 8, 1]
 print(f"Largest number =  {max(numbers)}")
@@ -19,6 +78,9 @@ numbers.sort()
 print(f"Largest number =  {numbers[-1]}")
 
 print(f"Largest number =  {sorted(numbers)[-1]}")
+
+# sum items in list
+print(f"sum of numbers = {sum(numbers)}")
 
 # 2D Lists
 print("\n2D Lists:")
