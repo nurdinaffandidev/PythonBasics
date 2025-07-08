d9bb4b76009fa861babedcd5b76ea24bfd7b5bb2
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-07-08 09:17:47 +0800

    chore: move import statment

--- Basics/MathFunction.py
+++ Basics/MathFunction.py
@@ -1,6 +1,7 @@
-import math # https://docs.python.org/3/library/math.html
 print(round(2.9))
 print(abs(-2.9))
+
+import math # https://docs.python.org/3/library/math.html
 
 print(math.ceil(2.9))
 print(math.floor(2.9))
