f0461b10d3170ec05dc97f637e748329a14702ee
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-07-21 14:12:25 +0800

    chore[ArithmeticOperations]: clean up print statements

--- Basics/ArithmeticOperations.py
+++ Basics/ArithmeticOperations.py
@@ -1,13 +1,19 @@
-print(10 + 3) # addition
-print(10 - 3) # subtraction
-print(10 * 3) # multiplication
-print(10 / 3) # division (float)
-print(10 // 3) # division (int)
-print(10 % 3) # modulo
-print(10 ** 3) # exponent
+print("============================")
+print("Arithmetic Operations: ")
+print("============================")
+print(f"addition: 10 + 3 = {10 + 3}") # addition
+print(f"subtraction: 10 - 3 = {10 - 3}") # subtraction
+print(f"multiplication: 10 * 3 = {10 * 3}") # multiplication
+print(f"division: 10 / 3 = {10 / 3}") # division (float)
+print(f"floor division: 10//3 = {10 // 3}") # division (int)
+print(f"modulo: 10 % 3 = {10 % 3}") # modulo
+print(f"exponent: 10 ** 3 = {10 ** 3}\n") # exponent
 
+print("augmented assignment operator:")
+print("-------------------------------")
 # augmented assignment operator
 x = 10
 x = x + 3
+print(f"x = x + 3 --> x = {x} ")
 x += 3 # augmented assignment operator
-print(x)
+print(f"x += 3 --> x = {x} ")
