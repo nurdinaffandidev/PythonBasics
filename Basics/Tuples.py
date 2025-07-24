25dd4ab78dc3da36f672368d14355a54d515ebe9
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-07-24 14:05:18 +0800

    chore[tuple]: update print statements

--- Basics/Tuples.py
+++ Basics/Tuples.py
@@ -1,31 +1,40 @@
 # Tuples are immutable
 # can get only information of tuple object, cannot change
+print("\nAccess value via index:")
+print("==============================")
 numbers = (1, 2, 3)
-print(numbers[0])
+print(f"value at index 0 = {numbers[0]}")
 
+# changing value of tuple will result in `TypeError`
 # numbers[0] = 10 # will result in `TypeError: 'tuple' object does not support item assignment`
 
 # Unpacking
+print("\nUnpacking:")
+print("==============================")
 coordinates = (1, 2, 3)
 # x = coordinates[0]
 # y = coordinates[1]
 # z = coordinates[2]
 x, y, z = coordinates # shorthand for above
-print(x, y, z)
-
-list_coordinates = [4, 5, 6]
-x, y, z = list_coordinates
-print(x, y, z)
-
+print(f"coordinates: {coordinates}")
+print(f"x, y, z = {x, y, z}")
+print(f"x = {x}")
+print(f"y = {y}")
+print(f"z = {z}")
 
 # empty tuple init
+print("\nEmpty tuple init:")
+print("==============================")
 empty_tuple = ()
 empty_tuple2 = tuple()
+print(empty_tuple)
+print(empty_tuple2)
 
 '''
-Mutability proof
+Immutability proof
 '''
-
+print("\nImmutability proof:")
+print("==============================")
 # Mutable
 list_1 =['History', 'Math', 'Science']
 list_2 = list_1
